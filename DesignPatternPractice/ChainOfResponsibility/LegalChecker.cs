84d7ed3d87fdc6cccd64dafee6c5fb392ad39189
Mehdi Heidari <hymehdi84@gmail.com>
2025-03-08 16:02:36 +0100

    first piece of chain of responsibtlity is done

---
+++ DesignPatternPractice/ChainOfResponsibility/LegalChecker.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatternPractice.ChainOfResponsibility;
+
+public abstract class LegalChecker
+{
+    protected LegalChecker? NextChecker = null;
+
+    public void SetNextChecker(LegalChecker nextChecker)
+    {
+        NextChecker = nextChecker;
+    }
+
+    public abstract bool Check();
+
+}
