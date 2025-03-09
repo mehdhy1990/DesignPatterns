277dff5b0ca12110e70e489c8f286d3c6dcce8bf
Mehdi Heidari <hymehdi84@gmail.com>
2025-03-09 13:16:00 +0100

    system for legal check is created

---
+++ DesignPatternPractice/ChainOfResponsibility/LegalCheckSystem.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatternPractice.ChainOfResponsibility;
+
+public class LegalCheckSystem
+{
+    private readonly LegalChecker _chain;
+
+    public LegalCheckSystem()
+    {
+        _chain = new IsContainDrug();
+        var pornChecker = new IsContainPornography();
+        var violenceChecker = new IsContainViolence();
+        _chain.SetNextChecker(new IsContainPornography());
+        pornChecker.SetNextChecker(new IsContainViolence());
+    }
+
+    public bool Check(Email email)
+    {
+        return _chain.Check(email);
+    }
+}
