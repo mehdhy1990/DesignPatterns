5201c2bffd36a4a3b2170b7c6dd946f22664f09a
Mehdi Heidari <hymehdi84@gmail.com>
2025-03-09 20:13:13 +0100

    progress is made

---
+++ DesignPatternPractice/Observer/MessageSystem.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatternPractice.Observer;
+
+public class MessageSystem : IMessageSystem
+{
+    private readonly List<IMessageObserver> _observers = [];
+    private string _textMessage = string.Empty;
+
+    public void SetState(string message)
+    {
+            _textMessage = message;        
+    }
+
+    public string GetState()
+    {
+        return _textMessage;
+    }
+    public void Attach(IMessageObserver observer)
+    {
+       _observers.Add(observer);
+    }
+
+    public void Detach(IMessageObserver observer)
+    {
+        _observers.Remove(observer);
+    }
+
+    public void Notify(IMessageObserver observer)
+    {
+        foreach (var item in _observers)
+        {
+            item.Update();
+        }
+    }
+}
---
+++ DesignPatternPractice/Observer/TextSystem.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatternPractice.Observer;
+
+public class TextSystem : IMessageObserver
+{
+    private readonly MessageSystem _messageSystem;
+
+    public TextSystem(MessageSystem messageSystem)
+    {
+        _messageSystem = messageSystem;
+    }
+
+    public void Update()
+    {
+        var state = _messageSystem.GetState();
+        Console.WriteLine($"the text system is reacted to {state}");
+    }
+}
