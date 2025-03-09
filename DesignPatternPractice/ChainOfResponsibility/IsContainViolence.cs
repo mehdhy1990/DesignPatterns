e7402baaf30d9c4b46596f0319acf74a52a52d6a
Mehdi Heidari <hymehdi84@gmail.com>
2025-03-09 13:10:27 +0100

    last chain is done

---
+++ DesignPatternPractice/ChainOfResponsibility/IsContainViolence.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatternPractice.ChainOfResponsibility;
+
+public class IsContainViolence : LegalChecker
+{
+    public override bool Check(Email email)
+    {
+        if(email.Body.Contains("blood") || email.Title.Contains("blood")) return true;
+        if( NextChecker is null ) return false;
+        return NextChecker.Check(email);
+    }
+}
