6fe35034f7ac1502d77cde315dee2bb795da35d6
Mehdi Heidari <hymehdi84@gmail.com>
2025-03-09 13:08:39 +0100

    next porn checker is done

---
+++ DesignPatternPractice/ChainOfResponsibility/IsContainPornography.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatternPractice.ChainOfResponsibility;
+
+public class IsContainPornography : LegalChecker
+{
+    public override bool Check(Email email)
+    {
+        if (email.Body.Contains("porn") || email.Title.Contains("porn"))
+        {
+            return true;
+        }
+        if(NextChecker is null) return false;
+        return NextChecker.Check(email);
+    }
+}
