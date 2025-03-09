cc420b01559204fd94c3e673b82cee1a137a5392
Mehdi Heidari <hymehdi84@gmail.com>
2025-03-09 20:26:54 +0100

    Observer pattern done

---
+++ DesignPatternPractice/Observer/EmailObserver.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatternPractice.Observer;
+
+public class EmailObserver : IMessageObserver
+{
+    private readonly MessageSystem _email;
+    public EmailObserver(MessageSystem messageSystem)
+    {
+        _email = messageSystem;
+    }
+    public void Update()
+    {
+        var email = _email.GetMessage();
+        if (email != "")
+        {
+            Console.WriteLine($"email reaction to : {email}");
+        }
+        
+    }
+}
