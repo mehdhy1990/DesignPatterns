b027e93d6ae30933a0b01ea393d37f5fae31acf4
Mehdi Heidari <hymehdi84@gmail.com>
2025-03-12 20:05:13 +0100

    singleton begin

--- DesignPatternPractice/obj/Debug/net9.0/DesignPatternPractice.AssemblyInfo.cs
+++ DesignPatternPractice/obj/Debug/net9.0/DesignPatternPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc420b01559204fd94c3e673b82cee1a137a5392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+547586b6dd5825a56fe883bd721119f6b0bc8c4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
