1457056709b1f37bf78b5429d52337144f5b5f8f
Mehdi Heidari <hymehdi84@gmail.com>
2025-03-15 12:51:07 +0100

    adapter pattern begin

---
+++ DesignPatternPractice/AdapterDesignPattern/Translator.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Channels;
+
+namespace DesignPatternPractice.AdapterDesignPattern;
+
+public class Translator
+{
+    public void TranslateLanguage()
+    {
+        Console.WriteLine("Hello from TranslateLanguage");
+    }
+}
+
+public class CzechLanguage()
+{
+    public void Speak()
+    {
+        Console.WriteLine("Hello from CzechLanguage");
+    }
+}
--- DesignPatternPractice/obj/Debug/net9.0/DesignPatternPractice.AssemblyInfo.cs
+++ DesignPatternPractice/obj/Debug/net9.0/DesignPatternPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cda17571c2f245d18191198257d58945994e961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec76037c6d3ca83b479f4d93b7d7d51facc0a06b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
