14b323133407e88fd4a543bcef4abcec282d8a0a
Mehdi Heidari <hymehdi84@gmail.com>
2025-04-09 11:50:55 +0200

    Mediator design pattern begins

--- DesignPatternPractice/obj/Debug/net9.0/DesignPatternPractice.AssemblyInfo.cs
+++ DesignPatternPractice/obj/Debug/net9.0/DesignPatternPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870a3bf9e2e3186d1a2d0d88b96de50cecb2ffcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c4b8e90872509afa8f88bfc4a57620c737b549")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPatternPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
