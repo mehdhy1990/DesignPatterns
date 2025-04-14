adcf02580541566adbcc19a1a3fe002cc360ce68
Mehdi Heidari <hymehdi84@gmail.com>
2025-04-14 19:02:27 +0200

    making mediator pattern failed we try once again

---
+++ DesignPatternPractice/Mediator/IChatRoom.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatternPractice.Mediator;
+
+public interface IChatRoom
+{
+    public void Register(TeamMember member);
+    public void SendMessage(string message);
+}
+
+public class TeamMember
+{
+    public IChatRoom? ChatRoom;
+
+    public string Name;
+
+    public TeamMember(string name)
+    {
+        Name = name;
+    }
+
+    internal void SetTeamMember(IChatRoom chatRoom)
+    {
+        ChatRoom = chatRoom;
+    }
+
+    
+}
