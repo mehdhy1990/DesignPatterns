24020efee816594f678531ea0ec5445ef733f800
Mehdi Heidari <hymehdi84@gmail.com>
2025-04-14 23:45:08 +0200

    Delegate completed succesfully

---
+++ CSharpFeatures/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpFeatures;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Action action = () => Console.WriteLine("Hello World!");
+       action();
+       Func<int, int,int> add = (x, y) => x + y;
+       Console.WriteLine(add(2, 3));
+       Predicate<int> isEven = x => x % 2 == 0;
+       Console.WriteLine(isEven(3));
+       CustomDelegate add2 = Add;
+       Console.WriteLine(add2(2, 3));
+    }
+
+    public delegate int CustomDelegate(int x, int y);
+
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+}
