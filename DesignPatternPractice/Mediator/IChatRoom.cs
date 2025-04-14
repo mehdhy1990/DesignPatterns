45cfb319fe0b0772c5e8257d74adf740a00fe114
Mehdi Heidari <hymehdi84@gmail.com>
2025-04-14 19:03:10 +0200

    making mediator pattern for the fourth time

--- DesignPatternPractice/Mediator/IChatRoom.cs
+++ DesignPatternPractice/Mediator/IChatRoom.cs
@@ -2,25 +2,5 @@
 
 public interface IChatRoom
 {
-    public void Register(TeamMember member);
-    public void SendMessage(string message);
-}
-
-public class TeamMember
-{
-    public IChatRoom? ChatRoom;
-
-    public string Name;
-
-    public TeamMember(string name)
-    {
-        Name = name;
-    }
-
-    internal void SetTeamMember(IChatRoom chatRoom)
-    {
-        ChatRoom = chatRoom;
-    }
-
     
 }
