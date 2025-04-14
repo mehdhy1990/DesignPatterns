e8d593671b2445d98bf57b1d6d15285da398959c
Mehdi Heidari <hymehdi84@gmail.com>
2025-04-14 18:36:41 +0200

    building mediator pattern contniues

---
+++ DesignPatternPractice/Mediator/Chatroom.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatternPractice.Mediator;
+
+public class Chatroom
+{
+    public Dictionary<string, TeamMember> Members = new();
+
+    public void Register(TeamMember member)
+    {
+        if (!Members.ContainsKey(member.Name))
+        {
+            Members.Add(member.Name, member);
+        }
+    }
+
+    public void SendMessage(string message)
+    {
+        Console.WriteLine("Hello to all team members");
+    }
+
+}
---
+++ DesignPatternPractice/Mediator/TeamMember.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatternPractice.Mediator;
+
+public class TeamMember
+{
+    public string Name { get; set; }
+    public void SendMesage(string message)
+    {
+        Console.WriteLine($"{this.GetType().Name}: {message}");
+    }
+
+    public virtual void ReceiveMesage()
+    {
+        Console.WriteLine($"{this.GetType().Name}: received the message");
+    }
+}
